8f30de0921c1e445a7962913541c721c0589e4b4
bavykin.grisha <bavykin.grisha@syk.fi>
2022-08-28 15:36:59 +0300

    fixed

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean weekday = true;
+        boolean vacation = false;
+
+        if (!weekday || vacation)
+        System.out.println("James sleeps");
+        else
+        System.out.println("James does not sleeps");
+
+    }
+}
